1726cdf183ae3ccc48518f9f1c151f46385ad897
José Luiz <joseluiz98.jl@gmail.com>
2018-10-12 19:02:21 -0300

    Sidebar espaçamento corrigido

--- resources/views/layouts/logged.blade.php
+++ resources/views/layouts/logged.blade.php
@@ -30,7 +30,7 @@
     <div class="container-fluid">
         <div class="row">
             <nav class="col-md-2 d-none d-md-block bg-light sidebar h-100 py-5">
-                <div class="sidebar-sticky py-3">
+                <div class="sidebar-sticky py-5">
                 <ul class="nav flex-column">
                     <li class="nav-item">
                     <a class="nav-link active" href="#">
