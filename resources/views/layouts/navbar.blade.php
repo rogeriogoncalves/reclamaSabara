383340520ad393f3af9eecccc29ed96dad7fc35a
José Luiz <joseluiz98.jl@gmail.com>
2018-10-15 14:31:32 -0300

    Logo e imagem na homepage corrigidos

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel fixed-top">
         <div class="container">
             <a class="navbar-brand" href="{{ url('/') }}">
-                <img src="{{ asset('images/logo.png') }}"/>
+                <img src="{{ asset('images/logo-verde.png') }}"/>
             </a>
 
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
