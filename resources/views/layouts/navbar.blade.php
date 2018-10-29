9389086b27dfb71f270a2de3f29d1c83c5e7ed26
José Luiz <joseluiz98.jl@gmail.com>
2018-10-30 02:16:51 +0530

    Link para login removido da navbar, 'register' traduzido

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -19,11 +19,8 @@
                     <!-- Authentication Links -->
                     @guest
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        <li class="nav-item">
                             @if (Route::has('register'))
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar-se') }}</a>
                             @endif
                         </li>
                     @else
